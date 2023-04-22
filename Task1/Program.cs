56f359c5d737813c9af468145a9b2c178dcb2679
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-22 21:41:34 +0200

    Задача 31

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -47,11 +47,13 @@ void Changenumbers(int[] numbers)
 void Findnumbers(int[] numbers, int num)
 {
     bool flag = false;
+    int index = -1;
     for (int i = 0; i < numbers.Length; i++)
   {
     if (numbers[i] == num)
     {
         flag = true;
+        index = i;
     }
   }
   if (flag) Console.WriteLine($"Число найдено. Оно стоит на {index + 1} позиции.");
@@ -60,45 +62,17 @@ void Findnumbers(int[] numbers, int num)
 
 //Задача 31: Задайте массив из 12 элементов, заполненный случайными
 //числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
-// void Task0()
-// {
-//     int size = 10;
-//     int[] numbers = new int[size];
-//     FillArray(numbers);
-//     PrintArray(numbers);
-//     GetPositiveSum(numbers);
-//     GetNegativeSum(numbers);
-//     Console.WriteLine($"Сумма положительных элементов равна {GetPositiveSum(numbers)}");
-//     Console.WriteLine($"Сумма отрицательных элементов равна {GetNegativeSum(numbers)}");
-// }
 
-//     методы
-// процедуры (не возвращают значение)   функции (возвращают значение)
 
-// Задача 32: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
-// [-4, -8, 8, 2] -> [4, 8, -8, -2]
-
-// void Task1()
-// {
-//     int size = 10;
-//     int[] numbers = new int[size];
-//     FillArray(numbers);
-//     PrintArray(numbers);
-//     Changenumbers(numbers);
-//     PrintArray(numbers);
-// }
-// Task1();
-
-// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-
-void Task2()
+void Task0()
  {
-    int size = 10;
-    int[] numbers = new int[size];
-    FillArray(numbers);
-    PrintArray(numbers);
-    Console.WriteLine("Введите число: ");
-    int currentNumber = Convert.ToInt32(Console.ReadLine());
-    Findnumbers(numbers, currentNumber);
+     int size = 10;
+     int[] numbers = new int[size];
+     FillArray(numbers);
+     PrintArray(numbers);
+     GetPositiveSum(numbers);
+     GetNegativeSum(numbers);
+     Console.WriteLine($"Сумма положительных элементов равна {GetPositiveSum(numbers)}");
+     Console.WriteLine($"Сумма отрицательных элементов равна {GetNegativeSum(numbers)}");
  }
- Task2();
+Task0();
